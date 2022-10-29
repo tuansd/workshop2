8e310d0578c7dade1add02f1b9b9a0a4207e5fd7
Tuan Tran <hometran@gmail.com>
2022-10-29 11:48:16 -0700

    added import to urls.py

--- app/nc_tutorials/urls.py
+++ app/nc_tutorials/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.config.urls.static import static
+from django.config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
